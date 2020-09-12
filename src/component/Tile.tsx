975d7017e4dfa3528d5a085154853ef18bff1a38
Carlos Salazar <carlosgsalazarf@gmail.com>
2020-09-12 15:45:36 -0500

    Getting assets in place and adding images

--- src/component/Tile.tsx
+++ src/component/Tile.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { TileProps } from "./Field";
+const bulbasaur = "bulbasaur.png";
+const finishTile = "finishtile.png";
+const grassTile = "grasstile.png";
+const rockTile = "rocktile.png";
 
 interface InputProps {
   tile: TileProps;
   click: Function;
 }
 
+const getImage = (tile: TileProps) => {
+  switch (tile) {
+    case TileProps.GRASS:
+      return process.env.PUBLIC_URL + grassTile;
+    case TileProps.START:
+      return process.env.PUBLIC_URL + bulbasaur;
+    case TileProps.END:
+      return process.env.PUBLIC_URL + finishTile;
+    case TileProps.OBSTACLE:
+      return process.env.PUBLIC_URL + rockTile;
+  }
+};
+
 const Tile = React.memo(({ tile, click }: InputProps) => {
   return (
     <div style={{ padding: "5px" }} className="tile" onClick={() => click()}>
-      {TileProps[tile]}
+      <img src={getImage(tile)} alt="" />
     </div>
   );
 });
